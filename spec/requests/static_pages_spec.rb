a3d561821810589b8ed96f35b40c480cf456428b
David Belavy <david@belavy.com>
2013-05-18 07:34:08 +1000

    Added home, help and about pages and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,4 +7,21 @@ describe "Static pages" do
       page.should have_content('Sample app')
     end
   end
+
+
+  describe "Help page" do
+  	it "should have the content 'Help page'" do
+  		visit '/static_pages/help'
+  		page.should have_content('Help page')
+  	end
+  end
+
+  describe "About page" do
+  	it "should have the content 'About us'" do
+  		visit '/static_pages/about'
+  		page.should have_content('About us')
+  	end
+  end
+
 end
+
