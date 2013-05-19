a71116b50ef5ac47d275504f8a9e8cb1d7d2f4e1
David Belavy <david@belavy.com>
2013-05-19 21:19:37 +1000

    Added page titles and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,26 +2,48 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "should have the content 'Sample app'" do
+    it "should have the h1 'Sample app'" do
       visit '/static_pages/home'
-      page.should have_content('Sample app')
+      page.should have_selector('h1', :text => 'Sample app')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text=>'Ruby on Rails Tutorial Sample App | Home')
+    end
+
   end
 
 
   describe "Help page" do
-  	it "should have the content 'Help page'" do
+
+  	it "should have the h1 'Help'" do
   		visit '/static_pages/help'
-  		page.should have_content('Help page')
-  	end
+  		page.should have_selector('h1', :text => 'Help')
+  	
+    end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text=>'Ruby on Rails Tutorial Sample App | Help')
+    end
+
   end
 
   describe "About page" do
-  	it "should have the content 'About us'" do
+  	it "should have the h1 'About us'" do
   		visit '/static_pages/about'
-  		page.should have_content('About us')
+  		page.should have_selector('h1', :text=>'About us')
   	end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text=>'Ruby on Rails Tutorial Sample App | About')
+    end
+
   end
 
 end
+
+
 
